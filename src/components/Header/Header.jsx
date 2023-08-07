9453c58d07b3bf2ad4326cc68b323b68b59f4865
Hasan Hasanzade <hasanzade.web@gmail.com>
2023-08-07 15:31:31 +0400

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,37 +7,38 @@ import styles from './header.module.scss'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
-         <img className={styles.image} src={logo} alt="logo" />
-         <div className={styles.text}>Organick</div>
-      </div>
-      <nav className={styles.nav}>
-         <ul className={styles.menu}>
-            <li className={styles.list}><a className={styles.item} href="/">Home</a></li>
-            <li className={styles.list}><a className={styles.item} href="/about">About</a></li>
-            <li className={styles.list}><a className={styles.item} href="/shop">Shop</a></li>
-            <li className={styles.list}><a className={styles.item} href="/services">Services</a></li>
-            <li className={styles.list}><a className={styles.item} href="/blogs">Blogs</a></li>
-         </ul>
-      </nav>
-      <div className={styles.bar}>
-         <div className={styles.search}>
-            <div className={styles.inputwr}>
-               <input className={styles.input} type="text" />
-               <div className={styles.outsearch}><AiOutlineSearch className={styles.aiout} color="white" size={24} /></div>  
-            </div>
-            
+      <div className={styles.body}>
+         <div className={styles.logo}>
+            <img className={styles.image} src={logo} alt="logo" />
+            <div className={styles.text}>Organick</div>
          </div>
-         <div className={styles.cart}>
-            <div className={styles.outcart}><AiOutlineShoppingCart color="white" size={24} /></div>
-            <div className={styles.name}>
-               Cart
-               <span className={styles.count}>(0)</span>
+         <nav className={styles.nav}>
+            <ul className={styles.menu}>
+               <li className={styles.list}><a className={styles.item} href="/">Home</a></li>
+               <li className={styles.list}><a className={styles.item} href="/about">About</a></li>
+               <li className={styles.list}><a className={styles.item} href="/shop">Shop</a></li>
+               <li className={styles.list}><a className={styles.item} href="/services">Services</a></li>
+               <li className={styles.list}><a className={styles.item} href="/blogs">Blogs</a></li>
+            </ul>
+         </nav>
+         <div className={styles.bar}>
+            <div className={styles.search}>
+            <button className={styles.search_icon}><AiOutlineSearch className={styles.aiout} color="white" size={24} /></button>  
+               <form className={styles.form}>
+                  <input className={styles.input} type="text" />
+                  <button className={styles.button}><AiOutlineSearch className={styles.aiout} color="white" size={24} /></button>  
+               </form> 
             </div>
+            <div className={styles.cart}>
+               <button className={styles.outcart}>
+                  <AiOutlineShoppingCart className={styles.shopping} color="white" size={24} />
+                  <span>0</span>
+               </button>
+            </div>
+            <a href='#' className={styles.login}>
+               <button>Login<AiOutlineLogin className={styles.aiout} ai size={24} /></button> 
+            </a>
          </div>
-         <a href='#' className={styles.login}>
-            <button>Login<AiOutlineLogin className={styles.aiout} ai size={24} /></button> 
-         </a>
       </div>
     </div>
   )
